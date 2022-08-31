de01b96a80127cd93c6a49802977899ccb7ccefe
kiyoshion <kiyoshion@gmail.com>
2022-09-01 02:04:39 +0900

    Update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,22 @@ import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className="sidebar-notes">
+        <div className="sidebar-note">
+          <div className="sidebar-note-title">
+            <h2>タイトル</h2>
+            <button>削除</button>
+          </div>
+          <p>内容</p>
+          <span>最終更新日</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
